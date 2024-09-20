f798baec41e7e87f42baa554da4c9145351b65e8
Hiren Marakana <hirenmarakana@Hirens-MacBook-Pro.local>
2024-09-21 00:25:06 +1000

    ashu

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('birthday-song');
 
-    // Safari fix: Allow audio autoplay after a small user interaction
+    // Safari fix: not needd Allow audio autoplay after a small user interaction
     document.body.addEventListener('click', () => {
         audio.play();
     }, { once: true });
