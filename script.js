0a0b4495a1046d28897fcb49212d5e89bf49328e
Hiren Marakana <hirenmarakana@Hirens-MacBook-Pro.local>
2024-09-20 23:38:21 +1000

    color

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*function playAudio() {
+function playAudio() {
     const audio = document.getElementById('birthday-song');
     audio.play().catch((error) => {
         console.error('Error playing audio:', error);
     });
     // Hide play button after playing
     document.querySelector('.play-button').style.display = 'none';
-}*/
+}
 // This script will ensure that the audio starts playing once the user interacts with the page
 window.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('birthday-song');
